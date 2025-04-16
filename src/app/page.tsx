a9c6ac7b9d3f69b3fbbc185c5ccfe4a428994451
Ayo <Ayo.meshe@gmail.com>
2025-04-16 18:40:41 +0100

    project init

---
+++ src/app/page.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const HomePage = () => {
+  return (
+    <div className="flex h-screen justify-center flex-row items-center bg-black">
+      <h1 className="text-6xl text-white font-extrabold">Home Page</h1>
+    </div>
+  );
+};
+
+export default HomePage;
